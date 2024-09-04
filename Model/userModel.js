347f9b5eca769f227307992d307cc63b4dcf39f7
EduardGhazaryan <137617257+EduardGhazaryan@users.noreply.github.com>
2024-09-04 12:24:11 +0400

    model

--- Model/userModel.js
+++ Model/userModel.js
@@ -1,39 +1,24 @@
-import mongoose from "mongoose";
-
-
-const userSchema = new mongoose.Schema({
-    phoneID:{type:String, required:true},
-    loans:[{type:mongoose.Schema.Types.ObjectId, ref:"Loan"}]
-})
-
-const User = mongoose.model("User", userSchema)
-
-const loanSchema = new mongoose.Schema({
-    phoneID:{type:String, required: true},
-    bankName:{type:String, required: true, enum:[ 'Ameriabank',
-        'Ardshinbank',
-        'Armeconombank',
-        'Artsakhbank',
-        'ACBA',
-        'AraratBank',
-        'Converse',
-        'HSBC',
-        'Inecobank',
-        'IDBank',
-        'Unibank',
-        'Evocabank',
-        'VTB',
-        'Byblos Bank',
-        'Mellat Bank',
-        'HayBusinessBank']},
-        amount:{type:Number,required:true},
-        dueDate: { type: Date, required: true },
-        endDate: { type: Date, required: true },
-        info: { type: String},
-        amountValute: { type: String, required: true },
-        paidStatus: { type: Boolean, default: false }
-})
-
-const Loan = mongoose.model("Loan", loanSchema)
-
+import mongoose from "mongoose"; 
+ 
+ 
+const userSchema = new mongoose.Schema({ 
+    phoneID:{type:String, required:true}, 
+    loans:[{type:mongoose.Schema.Types.ObjectId, ref:"Loan"}] 
+}) 
+ 
+const User = mongoose.model("User", userSchema) 
+ 
+const loanSchema = new mongoose.Schema({ 
+    phoneID:{type:String, required: true}, 
+    bankName:{type:String, required: true}, 
+        amount:{type:Number,required:true}, 
+        dueDate: { type: Date, required: true }, 
+        endDate: { type: Date, required: true }, 
+        description: { type: String}, 
+        amountValute: { type: String, required: true }, 
+        paidStatus: { type: Boolean, default: false } 
+}) 
+ 
+const Loan = mongoose.model("Loan", loanSchema) 
+ 
 export{Loan,User}
